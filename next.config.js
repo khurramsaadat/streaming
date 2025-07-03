c95e983b060f8fba6162d87e6040cdb524aa672c
khurram <khurram.saadat@yahoo.com>
2025-07-04 00:16:55 +0400

    chore: allow m.media-amazon.com as remote image source for next/image

---
+++ next.config.js
@@ -0,0 +1,13 @@
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'm.media-amazon.com',
+        pathname: '/images/**',
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig; 
